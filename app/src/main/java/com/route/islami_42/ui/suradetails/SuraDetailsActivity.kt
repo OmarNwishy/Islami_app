3727ed288558df4ad8866cb8f79c9ad4382c71bc
John Safwat <106314101+john-safwat@users.noreply.github.com>
2025-07-18 15:42:23 +0300

    Update SuraDetailsActivity.kt

--- app/src/main/java/com/route/islami_42/ui/suradetails/SuraDetailsActivity.kt
+++ app/src/main/java/com/route/islami_42/ui/suradetails/SuraDetailsActivity.kt
@@ -14,6 +14,7 @@ class SuraDetailsActivity : AppCompatActivity() {
 
     lateinit var sura: SuraDM
 
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySuraDetailsBinding.inflate(layoutInflater)
